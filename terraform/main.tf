8ef1d5dbce9365e1aa798f4c18ee7ede83b44c14
Cosmo <rluke42@hotmail.co.uk>
2025-12-28 21:32:42 +0000

    keyvault added

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+    backend "azurerm" {
+    resource_group_name = "terraform-deploy"  # Replace with your resource group name
+    storage_account_name = "terraformsflg"  # Replace with your storage account name
+    container_name = "aks-microservices-platform"  # Replace with your desired container name
+    key = "terraform.tfstate"  # Optional: Specify the filename within the container (defaults to 'terraform.tfstate')
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.75.0"
+    }
+  }
+}
+
+module "azurerm_key_vault" {
+  source = "./modules/security"
+  keyvaultname                = var.keyvaultname
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = var.tenant_id
+  spobject_id                 = var.spobject_id
+}
